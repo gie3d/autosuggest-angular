ee4359c79ec79194894f5a93892adf94f91740b1
gie3d <gie68i@gmail.com>
2015-02-10 16:27:03 +0700

    improve unit test

--- spec/autosuggest.unittest.js
+++ spec/autosuggest.unittest.js
@@ -17,11 +17,38 @@ describe('autoSuggestApp unit test', function() {
 			it("should be defined", function(){
 				expect(factory).toBeDefined();
 			});
+			it("should define displayedRic", function(){
+				expect(factory.getDisplayedRics().list.length > 0).toBeTruthy();
+			});
+			it("should define dataSource", function(){
+				expect(factory.getDataSource().rics.length > 0).toBeTruthy();
+			});
+		});
+
+		describe("factory functions", function(){
+			it("should be able to add new RIC to displayedRic list", function(){
+				var currentRicDisplayed = factory.getDisplayedRics().list.length;
+				factory.addRic({"ric": "g", "title":"test"});
+				expect(factory.getDisplayedRics().list.length).toEqual(currentRicDisplayed+1);
+			});
+			it("should be able to remove RIC from displayedRic list", function(){
+				var currentRicDisplayed = factory.getDisplayedRics().list.length;
+				factory.removeRic(0);
+				expect(factory.getDisplayedRics().list.length).toEqual(currentRicDisplayed-1);
+			});
+			it("should be able to remove all RIC from displayedRic list", function(){
+				factory.removeAllRics();
+				expect(factory.getDisplayedRics().list.length).toEqual(0);
+			});
+			it("should be able to detect Ric duplication", function(){
+				expect(factory.isRicDup('TRI.TO').result).toBeTruthy();
+				expect(factory.isRicDup('TRI.TO').index).toEqual(5);
+			});
 		});
 	});
 
 	describe('selectedRicsCtrl', function(){
-		var $rootScope, $scope, controller;
+		var $rootScope, $scope, controller, factory;
 
 		beforeEach(function() {
 			inject(function($injector){
@@ -65,9 +92,6 @@ describe('autoSuggestApp unit test', function() {
 				$scope.setPage(1);
 				expect($scope.isPrevShown()).toEqual('enabled');
 			});
-			it("should display a different RIC if the page is changed", function(){
-
-			});
 		});
 	});
 
@@ -78,6 +102,7 @@ describe('autoSuggestApp unit test', function() {
 			inject(function($injector){
 				$rootScope = $injector.get('$rootScope');
 				$scope = $rootScope.$new();
+				factory = $injector.get('autoSuggestFactory');
 				controller = $injector.get('$controller')('autoSugesstCtrl', {$scope: $scope});
 			});
 		});
@@ -85,6 +110,33 @@ describe('autoSuggestApp unit test', function() {
 		describe("Initialization", function(){
 			it("should be defined", function(){
 				expect(controller).toBeDefined();
+			});
+		});
+
+		describe("autosuggest function", function(){
+			it("should return related RIC", function(){
+				expect($scope.searchRic('IBM')[0].ric).toEqual('IBM.N')
+				
+			});
+			it("should add RIC to displayedRic list, if it is not duplicated", function(){
+				var currentRicDisplayed = factory.getDisplayedRics().list.length;
+				$scope.addRic("ric","title");
+				expect(factory.getDisplayedRics().list.length).toEqual(currentRicDisplayed+1);
+				expect($scope.duplicateRicAlert).toBe(false);
+			});
+			it("should be able to detect Ric duplication", function(){
+				var currentRicDisplayed = factory.getDisplayedRics().list.length;
+				$scope.addRic("TRI.TO","Thomson Reuters Corp");
+				expect(factory.getDisplayedRics().list.length).toEqual(currentRicDisplayed);
+				expect($scope.duplicateRicAlert).toBe(true);
+			});
+			it("should display suggestion box if user starts typing", function(){
+				$scope.searchRic('IBM');
+				expect($scope.displaySearchBox).toEqual(true)
+			});
+			it("should hide suggestion box if user clicks at any suggested ric", function(){
+				$scope.addRic("TRI.TO","Thomson Reuters Corp");
+				expect($scope.displaySearchBox).toEqual(false)
 			});
 		});
 	});
